6c88cc04bc5ab69d7c2b8c12ed715b2ec7286267
Ryan Andrews <rcandrews44@gmail.com>
2021-03-28 23:26:32 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+// Code your solutions in this file 
+const newArr = [];
+
+function writeCards(names, event){
+    for (let i = 0; i < names.length; i++) {
+        
+         newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+        
+    }
+ return newArr;
+} 
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer)
+        integer--
+    }
+
+}
